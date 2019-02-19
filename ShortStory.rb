0afad0bf232ff6c7958955f79e790ee8b0a0ccd5
Brandon Evans <bnevans@fb.com>
2019-02-19 13:52:06 -0600

    testing from work

--- ShortStory.rb
+++ ShortStory.rb
@@ -1,2 +1,3 @@
 puts "Now, this is a story all about how. My life got flipped-turned upside down. And Id like to take a minute. Just sit right there. Ill tell you how I became the. Prince of a town called Bel Air."
 puts "This is a song by Will Smith or something"
+puts "another test from work. still trying to figure out git and github"
